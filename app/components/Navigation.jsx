07f5a567180cc6c997e173f5a83904359cd1351c
MarlonG <mgacrama_ccs@uspf.edu.ph>
2024-05-15 18:28:14 +0800

    chore: Add Hero component and update Navigation styling

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import Image from "next/image";
 import logo from "@/public/logo.png";
+import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <div className="bg-[#969696] w-screen flex flex-row items-center justify-between px-14 py-4">
+    <div className="bg-[#ffffff7e] w-screen flex flex-row items-center justify-between px-14 py-4 fixed top-0 z-20">
       <div>
         <Image src={logo} alt="logo" width={150} height={150} />
       </div>
       <div className="flex">
         <ul className="flex justify-center space-x-4">
-          <li>
-            <a href="#" className="text-white">
+          <li className="cursor-pointer">
+            <Link href="#" className="text-white ">
               HOME
-            </a>
+            </Link>
           </li>
-          <li>
-            <a href="#" className="text-white">
+          <li className="cursor-pointer">
+            <Link href="#" className="text-white ">
               ABOUT
-            </a>
+            </Link>
           </li>
-          <li>
-            <a href="#" className="text-white">
+          <li className="cursor-pointer">
+            <Link href="#" className="text-white">
               CONTACT
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
--- app/page.js
+++ app/page.js
@@ -2,12 +2,16 @@ import React from "react";
 import Navigation from "./components/Navigation";
 import Image from "next/image";
 import logo from "@/public/logo.png";
+import Hero from "./Hero";
 
 const page = () => {
   return (
     <div className="relative">
       <div>
         <Navigation />
+      </div>
+      <div>
+        <Hero />
       </div>
     </div>
   );
