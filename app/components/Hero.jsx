560719625fbe58536009e0966887497609cf355f
MarlonG <mgacrama_ccs@uspf.edu.ph>
2024-05-15 18:51:42 +0800

    chore: Refactor Hero component and update font in layout

---
+++ app/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="h-screen relative flex flex-col gap-8">
+      <div className="bg-[#000000be] opacity-60 h-full w-full absolute top-0 left-0 z-10" />
+      <video
+        src="/gaming.webm"
+        autoPlay
+        loop
+        muted
+        className="w-full h-full object-cover absolute top-0 left-0 z-0"
+      />
+      <div className="z-20 flex flex-col justify-center items-center h-full absolute top-0 left-0 w-full text-white">
+        <h1 className="text-7xl font-light ">The Task Masters</h1>
+        <p className="text-2xl mt-10">Built by the Virtual Workforce</p>
+        <p className="text-2xl">For the Virtual Workforce</p>
+        <div className="mt-10 z-30">
+          <button className="bg-transparent border-white border text-white hover:bg-white hover:text-black px-12 py-4 rounded-lg font-bold text-xl transition-all ease-in-out duration-300">
+            Learn more
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/page.js
+++ app/page.js
@@ -2,11 +2,11 @@ import React from "react";
 import Navigation from "./components/Navigation";
 import Image from "next/image";
 import logo from "@/public/logo.png";
-import Hero from "./Hero";
+import Hero from "./components/Hero";
 
 const page = () => {
   return (
-    <div className="relative">
+    <div>
       <div>
         <Navigation />
       </div>
